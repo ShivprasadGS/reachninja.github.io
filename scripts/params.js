3bd2ca61e7f8d03e37b063dcfa949e6078fb1fea
Shivprasad Ganesh Shivratri <shivprasadshivratri8793@gmail.com>
2025-07-21 21:54:17 -0400

    start of the project

---
+++ scripts/params.js
@@ -0,0 +1,19 @@
+const params = {
+  width: 640,
+  height: 480,
+  infoHeight: 50,
+  pixelSize: 20,
+  redMarkerRadius: 10,
+  redMarkerInit: [320, 240],
+  gameDuration: 40,
+  FPS: 60,
+  blueScore: 1,
+  blackScore: -1,
+  blue: 'blue',
+  black: 'black',
+  red: 'red',
+  white: 'white',
+  backgroundColor: '#f5f5f5',
+  dirChange: false,
+  playerName: 'Player02'
+};
